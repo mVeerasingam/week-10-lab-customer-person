58ac265941c190e667f989e16f13802cb20d31c5
G00403931 <G00403931@ATU.ie>
2022-11-29 13:31:05 +0000

    first commit

---
+++ src/main/java/ie/atu/week10/Person.java
@@ -0,0 +1,10 @@
+package ie.atu.week10;
+
+import java.util.Scanner;
+
+public class Person {
+    public static void main(String[] args) {
+        Person_Instance Dave=new Person_Instance("Dave", "Mervue", "12345");
+        System.out.println(Dave.getName());
+    }
+}
---
+++ src/main/java/ie/atu/week10/Person_Instance.java
@@ -0,0 +1,37 @@
+package ie.atu.week10;
+
+public class Person_Instance {
+    private String name;
+    private String address;
+    private String tele;
+
+    public Person_Instance(){
+    }
+
+    public Person_Instance(String name, String address, String tele){
+        this.name=name;
+        this.address=address;
+        this.tele=tele;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name=name;
+    }
+
+    public String getAddress(){
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getTele() {
+        return tele;
+    }
+    public void setTele(String tele) {
+        this.tele = tele;
+    }
+}
